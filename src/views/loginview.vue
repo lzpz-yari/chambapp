2e2c14cb83ece72c0b04b2e2868f2a04596e56a3
eduardovalencia501 <eduardovalencia501@gmail.com>
2026-04-22 10:08:17 -0600

    prueba de commit

--- src/views/loginview.vue
+++ src/views/loginview.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex">
 
-    <!-- Panel izquierdo — Formulario -->
+    <!-- Panel izquierdo — Formularioo -->
     <div class="flex flex-col flex-1 w-full lg:w-1/2 overflow-y-auto px-6 py-10 bg-white">
       <div class="w-full max-w-md mx-auto mb-6">
         <a href="#" class="inline-flex items-center text-sm text-gray-500 hover:text-gray-700 transition-colors">
